b3331c069954bf46f6c4fbc70be9700ab63d1ff8
Yisheng Jiang <yishengjiang@yahoo.com>
2026-02-14 16:21:09 -0800

    vite bas

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/gbk/" : "/",
+  base: "/gbk/"
 }));
